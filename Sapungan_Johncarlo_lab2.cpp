88893e971d9d24b5a485bb4e9bb2f29a21e50f9b
Sapungan-Johncarlo <sapunganjohncarlo34@gmail.com>
2025-03-15 17:36:53 +0800

    acvtivity 2 part 2

---
+++ Sapungan_Johncarlo_lab2.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+using namespace std;
+
+typedef struct Node{
+        string songName;
+        Node *link;
+}Node;
+
+Node* createNode(string data) {
+    Node* newNode = new Node;
+    newNode->songName = data;
+    newNode->link = NULL;
+    return newNode;
+}
+void traverse (Node *head){
+    Node *temp = new Node;
+    temp = head;
+    
+    cout << "My Playlist" <<endl;
+    while(temp !=NULL){
+        cout << temp->songName<< "->" <<endl;
+        if(temp->link == NULL){
+            cout << "NULL" << endl;
+    }
+    temp = temp->link;
+}
+}
+Node *insertAtEnd(string data, Node *head){
+    if(head == NULL){
+        Node *newNode = createNode(data);
+        head = newNode;
+        cout << "A new node has been insterted at the end \n" << endl;
+        return head;
+    }
+    Node *temp = new Node;
+    temp = head;
+    
+    while (temp->link != NULL){
+        temp = temp->link;
+    }
+    Node *newNode = createNode(data);
+    temp->link = newNode;
+    
+    cout << "A new node has been insterted at the end \n" << endl;
+    return head;
+}
+Node *instertAtBeginning(string data, Node *head){
+    Node *newNode = createNode(data);
+    newNode->link = head;
+    
+    head = newNode;
+    
+    cout << "A new node has been inserted at the beginning \n" <<endl;
+    return head;
+}
+string insertAfter(string after, string data, Node *head){
+    Node *temp = new Node; 
+    temp = head; 
+    
+    while(temp->songName.compare(after) !=0){
+        if(temp == NULL){
+            return "No such song exist, please try again.";
+        }
+        
+        temp = temp->link;
+    }
+    Node *newNode = createNode(data);
+    newNode->link = temp->link;
+    temp->link = newNode;
+    
+    return "An new node has been added after" + after + "\n";
+}
+string deleteAtEnd(Node *head){
+    if(head == NULL){ 
+        return "The linked list is empty \n";
+    }
+
+    if(head->link == NULL){ 
+        delete head;
+        return "The head has been deleted \n";
+    }
+
+    Node *temp = new Node; 
+    temp = head; 
+
+    while(temp->link->link != NULL){ 
+        temp = temp->link;
+    }
+
+    temp->link = NULL; 
+
+    return "A node has been deleted at the end \n";
+}
+Node *deleteFromBeginning(Node *head){
+    if(head == NULL){
+        cout << "The linked list is empty \n" << endl;
+        return NULL;
+    }
+    
+    if(head->link == NULL){
+        delete head;
+    }
+    head = head->link;
+    
+    cout << "A node has been delete from the beginning \n" << endl;
+    
+    return head;
+}
+Node *deleteFromGivenNode(string givenNode, Node *head){
+    if(head == NULL){
+        cout << "The Node" + givenNode + " has been deleted. \n" << endl;
+        return head;
+    }
+    Node *temp = new Node;
+    Node *next = new Node;
+    temp = head;
+    next = temp->link;
+    
+    while(next->songName.compare(givenNode) !=0){
+        if(temp == NULL){
+            cout << "No Such node exist. \n" << endl;
+            return head;
+    }
+    next = next->link;
+    temp = temp->link;
+}
+    temp->link = next->link;
+    cout << "The Node " + givenNode + " has been deleted. \n" << endl;
+    return head;
+}
+int main()
+{
+    Node *head = createNode ("Take all the love by Arthur Nery");
+    traverse(head);
+    head = insertAtEnd("Isa lang by arthur Nery", head);
+    traverse(head);
+    head = insertAtEnd("Pagsamo by Arthur Nery", head);
+    traverse(head);
+    head = insertAtEnd("Binhi by Arthur Nery", head);
+    traverse(head);
+    
+    head = instertAtBeginning ("Higa by Arthur Nery", head);
+    traverse(head);
+    head = instertAtBeginning ("Sinag by Arthur Nery", head);
+    traverse(head);
+    head = instertAtBeginning ("Life Puzzle by Arthur Nery", head);
+    traverse(head);
+    head = instertAtBeginning ("O ninanais by Arthur Nery:", head);
+    traverse(head);
+    
+    insertAfter ("Higa by Arthur Nery", "Ikaw at ako by tj Monterde", head);
+    traverse(head);
+    insertAfter ("Sinag by Arthur Nery", "Kung sya man by tj Monterde", head);
+    traverse(head);
+    insertAfter ("Life Puzzle by Arthur Nery", "Dating Tayo by tj Monterde", head);
+    traverse(head);
+    
+    cout << deleteAtEnd(head);
+    traverse(head);
+    head = deleteFromBeginning(head);
+    traverse(head);
+    head = deleteFromGivenNode("Dating Tayo by tj Monterde ",head);
+    traverse(head);
+    
+    return 0;
+}
